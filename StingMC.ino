b27585730727e779f742d7437a214068434b7f1c
PonderForge <yotoby61@gmail.com>
2026-04-06 20:55:08 -0400

    FinBefore

--- StingMC.ino
+++ StingMC.ino
@@ -1,26 +1,27 @@
-#include "MAX31855.h"
+#include <MAX31855.h>
 #include <EEPROM.h>
 
 #define uCModeLED 11
 #define uCStatusLED 10
 #define uCErrorLED 9
-#define throttleProg 12
-#define throttle 27
-#define mcin1 0
-#define mcin2 1
-#define mcen 2
+#define throttleProgSW 12
+#define throttleIn 27
+#define MCIn1 0
+#define MCIn2 1
+#define MCEN 2
 #define nFault 4
+#define MCCurrent 3
 
 #define SCK_PIN  22
 #define CS_PIN   21
 #define MISO_PIN 20
 
 MAX31855 tc(SCK_PIN, CS_PIN, MISO_PIN);
-int throttleMin = 300;
-int throttleMax = 900;
+int throttleMin = 400;
+int throttleMax = 800;
 
 void setup() {
-  // Start up Code
+  // Boot Code900
   tc.begin();
   EEPROM.begin(256);
   Serial.begin(115200);
@@ -29,16 +30,17 @@ void setup() {
   pinMode(uCModeLED, OUTPUT);
   pinMode(uCStatusLED, OUTPUT);
   pinMode(uCErrorLED, OUTPUT);
-  pinMode(throttleProg, INPUT);
-  pinMode(throttle, INPUT);
+  pinMode(throttleProgSW, INPUT);
+  pinMode(throttleIn, INPUT);
   pinMode(nFault, INPUT);
-  pinMode(mcin2, OUTPUT);
-  pinMode(mcin1, OUTPUT);
-  pinMode(mcen, OUTPUT);
+  pinMode(MCIn2, OUTPUT);
+  pinMode(MCIn1, OUTPUT);
+  pinMode(MCEN, OUTPUT);
+  pinMode(MCCurrent, INPUT);
 
   // Set MC to be active
-  digitalWrite(mcin1, LOW);
-  digitalWrite(mcen, HIGH);
+  digitalWrite(MCIn1, LOW);
+  digitalWrite(MCEN, HIGH);
 
   // Enable MC IN1 PWM
   analogWriteFreq(10000);
@@ -48,12 +50,16 @@ void setup() {
   Serial.print(tc.getInternalTemperature());
   Serial.println(" Celsius");
 
-  int min = (EEPROM.read(0) << 8) | EEPROM.read(1);
-  if (min != 0) {
+  // Read Throttle Bounds into Memory
+  int min = makeWord(EEPROM.read(0), EEPROM.read(1));
+  if (min <= 1024) {
     throttleMin = min;
+  } else {
+    throttleMin = analogRead(throttleIn);
   }
-  int max = (EEPROM.read(2) << 8) | EEPROM.read(3);
-  if (max != 0) {
+  int max = makeWord(EEPROM.read(2), EEPROM.read(3));
+  Serial.println(max);
+  if (max <= 1024) {
     throttleMax = max;
   }
 
@@ -61,43 +67,61 @@ void setup() {
 }
 
 void loop() {
-  int drive = map(analogRead(throttle), throttleMin, throttleMax, 0, 255);
+  // Change Speed to Throttle
+  int drive = map(analogRead(throttleIn), throttleMin, throttleMax, 0, 255);
   if (drive < 0)
     drive = 0;
   if (drive > 255)
     drive = 255;
-  Serial.print("value: ");
-  Serial.println(drive);
-  analogWrite(mcin2, drive);
+  analogWrite(MCIn2, drive);
 
+  // Check for Configuration reqiurements
   throttleConfig();
 
-  while (digitalRead(nFault)) {
+  // Drive Fault Checking
+  while (!digitalRead(nFault)) {
     digitalWrite(uCErrorLED, HIGH);
-    analogWrite(mcin2, 0);
+    analogWrite(MCIn2, 0);
+    delay(100);
+    digitalWrite(uCErrorLED, LOW);
   } 
 
-  delay(100);
+  // Heartbeat
+  digitalWrite(uCStatusLED, HIGH);
+  delay(25);
+  digitalWrite(uCStatusLED, LOW);
+  delay(25);
+
+  // Show Current consumption on ErrorLED
+  digitalWrite(uCErrorLED, digitalRead(MCCurrent));
 }
 
 void throttleConfig () {
-  if (digitalRead(throttleProg)) {
-    analogWrite(mcin2, 0);
+  if (digitalRead(throttleProgSW)) {
+    analogWrite(MCIn2, 0);
     digitalWrite(uCModeLED, LOW);
-    digitalWrite(uCStatusLED, LOW);
+    throttleMin = 400;
+    throttleMax = 800;
 
-    while (digitalRead(throttleProg)) {
-      int val = analogRead(throttle);
+    // Find Max and Min of Throttle
+    while (digitalRead(throttleProgSW)) {
+      digitalWrite(uCStatusLED, LOW);
+      int val = analogRead(throttleIn);
       if (val > throttleMax) {
         throttleMax = val;
+        digitalWrite(uCStatusLED, HIGH);
       } else if (val < throttleMin) {
         throttleMin = val;
+        digitalWrite(uCStatusLED, HIGH);
       }
+      delay(100);
     }
 
+    // Add Padding to Throttle
     throttleMax = throttleMax - 50;
     throttleMin = throttleMin + 50;
 
+    // Write Throttle Bounds to Flash
     EEPROM.write(0, (throttleMin >> 8) & 0xFF);
     EEPROM.write(1, throttleMin & 0xFF);
     EEPROM.write(2, (throttleMax >> 8) & 0xFF);
